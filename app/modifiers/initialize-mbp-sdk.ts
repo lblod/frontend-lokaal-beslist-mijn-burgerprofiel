ae8bcae19f7cb33986d1e809e699c5d9158b6b1b
Andres Vergauwen <andres.vergauwen@hotmail.com>
2025-04-15 15:22:57 +0200

    Add MBP SDK integration and update project dependencies
    - Introduced `initialize-mbp-sdk` modifier for embedding MBP SDK.
    - Added `MbpEmbed` component to application template.
    - Changed location type in environment configuration to 'none'.
    - Updated package.json and package-lock.json to include @govflanders/mbp-embed-sdk dependency.

---
+++ app/modifiers/initialize-mbp-sdk.ts
@@ -0,0 +1,25 @@
+import { modifier } from 'ember-modifier';
+import type { MbpEmbedClient } from '@govflanders/mbp-embed-sdk';
+import { createMbpEmbedClient } from '@govflanders/mbp-embed-sdk';
+
+export default modifier(function initializeMbpSdk(
+  element: HTMLElement,
+  [clientId]: [string],
+) {
+  (async () => {
+    const client: MbpEmbedClient = createMbpEmbedClient(clientId, {
+      allowedHosts: [
+        'https://*.burgerprofiel.be/*',
+        'https://admin.vlaanderen.be/*',
+      ],
+      element,
+    });
+
+    try {
+      await client.connect();
+      console.log('MBP SDK connected!');
+    } catch (e) {
+      console.error('MBP SDK connection failed:', e);
+    }
+  })();
+});
