f1c2bcb73be11c9403781920753dcc5697fe0987
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-05-08 14:46:54 +0200

    bnb-871: create select radio button filter

---
+++ app/components/filters/select-radio-button-filter.ts
@@ -0,0 +1,30 @@
+import { action } from '@ember/object';
+import FilterComponent, { type FilterArgs } from './filter';
+
+type GroupedOptions = {
+  groupName: string;
+  option: string;
+};
+
+type UnifiedOptions = string | GroupedOptions;
+interface Signature {
+  Args: {
+    options: Promise<UnifiedOptions[]>;
+    selected: string;
+    updateSelected: (selected?: string) => void;
+  } & FilterArgs;
+}
+
+export default class SelectRadioButtonFilterComponent extends FilterComponent<Signature> {
+  get selected() {
+    return this.args.selected;
+  }
+
+  @action
+  async selectChange(selectedOption: string) {
+    this.args.updateSelected(selectedOption);
+    this.updateQueryParams({
+      [this.args.queryParam]: selectedOption,
+    });
+  }
+}
