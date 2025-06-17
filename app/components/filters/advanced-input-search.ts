8efc29188359856c7cef4e2c0e894fb2135eacfc
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-06-17 16:57:33 +0200

    bnb-921: add modal for more info about the advanced search

--- app/components/filters/advanced-input-search.ts
+++ app/components/filters/advanced-input-search.ts
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export interface FiltersAdvancedInputSearchSignature {
   Args: {
@@ -9,8 +10,15 @@ export interface FiltersAdvancedInputSearchSignature {
 }
 
 export default class FiltersAdvancedInputSearch extends Component<FiltersAdvancedInputSearchSignature> {
+  @tracked isInfoModalOpen = false;
+
   @action
   updateKeyword(event: { target: { value?: string } }) {
     this.args.onUpdateKeyword?.(event.target.value || null);
+  }
+
+  @action
+  closeModal() {
+    this.isInfoModalOpen = false;
   }
 }
