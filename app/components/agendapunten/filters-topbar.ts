b455cf7b3b917b022002ff415a5a114dd7ad02cc
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2025-06-18 16:58:57 +0200

    BNB-894 | selected date is shown as one label in topbar (#14)
    * bnb-894: split up the formatting of the filter to a topbar label
    
    * bnb-894: the start and end is shown as one period label
    
    * bnb-894: you can also remove the period label
    
    * bnb-894: no need for assigning the distanceValue to a const

--- app/components/agendapunten/filters-topbar.ts
+++ app/components/agendapunten/filters-topbar.ts
@@ -17,6 +17,8 @@ export interface AgendapuntenFiltersTopbarSignature {
   Element: null;
 }
 
+const PERIOD_KEY = 'periode';
+
 export default class AgendapuntenFiltersTopbar extends Component<AgendapuntenFiltersTopbarSignature> {
   @service declare itemsService: ItemsService;
   @service declare filterService: FilterService;
@@ -27,38 +29,117 @@ export default class AgendapuntenFiltersTopbar extends Component<AgendapuntenFil
   }
 
   get filterValues() {
-    return Object.entries(this.args.filters)
-      ?.map(([key, value]) => {
-        if (key == QueryParameterKeys.municipalities) {
-          return {
-            key: null,
-            value: null,
-          };
-        }
-        if (key == QueryParameterKeys.distance && value) {
-          const distanceOption = this.distanceList.getSelectedDistance(value);
-          if (distanceOption) {
-            return {
-              key,
-              value: distanceOption.label,
-            };
-          }
-          return {
-            key: null,
-            value: null,
-          };
-        }
-        return {
-          key,
-          value: value,
-        };
-      })
-      .filter((kv) => kv.value && kv.value !== '');
+    return this.filterKeys
+      .map((key) => this.getFormattedLabelForFilter(key))
+      .filter((kv) => kv);
+  }
+
+  get filterKeys() {
+    const keysOfFilters = Object.keys(this.args.filters);
+    const keysWithValue = keysOfFilters.filter((key) => {
+      const filterValue = this.args.filters[key as keyof FiltersAsQueryParams];
+      if (!filterValue || filterValue.trim?.() === '') {
+        return false;
+      }
+
+      if (this.hiddenFilterKeys.includes(key)) {
+        return false;
+      }
+
+      return true;
+    });
+    if (
+      keysOfFilters.includes(QueryParameterKeys.start || QueryParameterKeys.end)
+    ) {
+      keysWithValue.push(PERIOD_KEY);
+    }
+
+    return keysWithValue;
+  }
+
+  get hiddenFilterKeys() {
+    return [
+      QueryParameterKeys.municipalities,
+      QueryParameterKeys.provinces,
+      QueryParameterKeys.start, // this is handled as one label with end
+      QueryParameterKeys.end, // this is handled as one label with start
+    ];
+  }
+
+  get keyFormatMapping() {
+    return {
+      [QueryParameterKeys.distance]: this.createDistanceFilterLabel(),
+      [PERIOD_KEY]: this.createPeriodFilterLabel(),
+    };
+  }
+
+  getFormattedLabelForFilter(key: string) {
+    if (Object.keys(this.keyFormatMapping).includes(key)) {
+      return this.keyFormatMapping[key];
+    }
+    const value = this.args.filters[key as keyof FiltersAsQueryParams];
+    if (!value) {
+      return null;
+    }
+
+    return {
+      key,
+      value: value,
+    };
+  }
+
+  createDistanceFilterLabel() {
+    const distanceValue = this.args.filters.afstand || undefined;
+    const distanceOption = this.distanceList.getSelectedDistance(distanceValue);
+    if (distanceOption && distanceValue) {
+      return {
+        key: QueryParameterKeys.distance,
+        value: distanceOption.label,
+      };
+    }
+    return null;
+  }
+
+  createPeriodFilterLabel() {
+    const start = this.args.filters.begin;
+    const end = this.args.filters.eind;
+
+    if (start && end) {
+      return {
+        key: PERIOD_KEY,
+        value: `Van ${start} tot ${end}`,
+      };
+    } else if (start) {
+      return {
+        key: QueryParameterKeys.start,
+        value: `Van ${start}`,
+      };
+    } else if (end) {
+      return {
+        key: QueryParameterKeys.end,
+        value: `Tot ${end}`,
+      };
+    }
   }
 
   @action
-  removeFilter(queryParamKey: keyof FiltersAsQueryParams) {
-    this.filterService.updateFilterFromQueryParamKey(queryParamKey, null);
+  removeFilter(queryParamKey: string) {
+    if (queryParamKey === 'periode') {
+      this.filterService.updateFilterFromQueryParamKey(
+        QueryParameterKeys.start as keyof FiltersAsQueryParams,
+        null,
+      );
+      this.filterService.updateFilterFromQueryParamKey(
+        QueryParameterKeys.end as keyof FiltersAsQueryParams,
+        null,
+      );
+    } else {
+      this.filterService.updateFilterFromQueryParamKey(
+        queryParamKey as keyof FiltersAsQueryParams,
+        null,
+      );
+    }
+
     this.args.onFiltersUpdated?.();
   }
 }
