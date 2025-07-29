7f1f2b13f9b1abe8bda7cbab2b23af9a0a2a0cc9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-07-29 09:56:23 +0200

    bnb-932: fix reset of filters in filter topbar

--- app/controllers/agenda-items/index.ts
+++ app/controllers/agenda-items/index.ts
@@ -8,19 +8,25 @@ import type RouterService from '@ember/routing/router-service';
 import type FilterService from 'frontend-burgernabije-besluitendatabank/services/filter-service';
 import type DistanceListService from 'frontend-burgernabije-besluitendatabank/services/distance-list';
 import type AddressService from 'frontend-burgernabije-besluitendatabank/services/address';
+import type GoverningBodyListService from 'frontend-burgernabije-besluitendatabank/services/governing-body-list';
+import type ItemListService from 'frontend-burgernabije-besluitendatabank/services/item-list';
 
 export default class AgendaItemsIndexController extends Controller {
   @service declare filterService: FilterService;
   @service declare distanceList: DistanceListService;
   @service declare address: AddressService;
+  @service declare governingBodyList: GoverningBodyListService;
+  @service('item-list') declare itemsService: ItemListService;
   @service declare router: RouterService;
   @tracked hasFilter = false;
 
   @action
   resetFilters() {
+    this.governingBodyList.selectedIds = [];
+    this.address.selectedAddress = undefined;
     this.distanceList.selected = null;
-    this.address.selectedAddress = null;
     this.filterService.resetFiltersToInitialView();
+    this.itemsService.fetchItems.perform(0, false);
     this.router.transitionTo(this.router.currentRouteName, {
       queryParams: this.filterService.resetQueryParams,
     });
