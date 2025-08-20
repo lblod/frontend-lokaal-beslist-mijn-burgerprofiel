70d7ea215ce106296dab048c6c294d3432a024e5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-08-20 14:53:19 +0200

    debug: set status loading false so sdk does not show loading state
    Refs: bnb-943

--- app/services/mbp-embed.ts
+++ app/services/mbp-embed.ts
@@ -136,6 +136,7 @@ export default class MbpEmbedService extends Service {
       }
       const queryParams = this.filterService.asUrlQueryParams;
       this.client.navigation.openNewEmbed(`${data.url}${queryParams}`);
+      this.client.ui.setStatusLoading(false);
     }
   }
 }
