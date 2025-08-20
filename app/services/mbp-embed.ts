580640748597bb026cb25ea31def53c1c35880c5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-08-20 09:57:48 +0200

    debug: fix not being send to overview page because of navigate back is triggered
    When selecitng a tile vlaanderen or aalter go get navigated back instantly as this is what the code said. Changed this so it needs to come from a detail page.
    
    Refs: bnb-943

--- app/services/mbp-embed.ts
+++ app/services/mbp-embed.ts
@@ -95,17 +95,18 @@ export default class MbpEmbedService extends Service {
       return;
     }
 
-    const openNewEmbedRouteNames = ['agenda-items.agenda-item'];
-
+    const openNewEmbedRouteNames = ['agenda-items.agenda-item', 'filter'];
     if (openNewEmbedRouteNames.includes(transition.to?.name ?? '')) {
       await this.client.navigation.openNewEmbed(
         'https://mbp.lokaalbeslist.lblod.info/3d814daa-cbe8-550b-9d41-f3caa9b2c2a5',
       );
     }
 
-    const navigateBackRouteNames = ['agenda-items.index'];
-
-    if (navigateBackRouteNames.includes(transition.to?.name ?? '')) {
+    const navigateBackRouteNames = ['agenda-items.index', 'sessions.session'];
+    if (
+      openNewEmbedRouteNames.includes(transition.from?.name ?? '') &&
+      navigateBackRouteNames.includes(transition.to?.name ?? '')
+    ) {
       await this.client.navigation.back();
     }
   }
