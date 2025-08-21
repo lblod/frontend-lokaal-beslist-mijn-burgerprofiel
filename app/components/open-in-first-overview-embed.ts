de6d62d03f08eb50e5dd61060019e5d8a5d732bd
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-08-21 08:50:34 +0200

    feat: create component to go to the first embed + test out logic
    Logic is still WIP as im not sure how it will react
    
    Refs: bnb-943

---
+++ app/components/open-in-first-overview-embed.ts
@@ -0,0 +1,44 @@
+import Component from '@glimmer/component';
+
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+import type MbpEmbedService from 'frontend-burgernabije-besluitendatabank/services/mbp-embed';
+import type RouterService from '@ember/routing/router-service';
+import type { FiltersAsQueryParams } from 'frontend-burgernabije-besluitendatabank/controllers/agenda-items/types';
+import type FilterService from 'frontend-burgernabije-besluitendatabank/services/filter-service';
+
+export interface OpenInFirstOverviewEmbedSignature {
+  Args: {
+    routeName: string;
+    query: Partial<FiltersAsQueryParams>;
+  };
+}
+
+export default class OpenInFirstOverviewEmbed extends Component<OpenInFirstOverviewEmbedSignature> {
+  @service declare mbpEmbed: MbpEmbedService;
+  @service declare router: RouterService;
+  @service declare filterService: FilterService;
+
+  @action
+  async openAsEmbed() {
+    if (this.mbpEmbed.isConnected) {
+      await this.recursiveBackTillOverview();
+      this.mbpEmbed.openNewEmbed({
+        routeName: this.args.routeName,
+      });
+    } else {
+      this.router.transitionTo(
+        this.args.routeName,
+        this.filterService.asQueryParams,
+      );
+    }
+  }
+
+  async recursiveBackTillOverview() {
+    await this.mbpEmbed.client.navigation.back();
+    console.log('1', this.router.currentRouteName);
+    await this.mbpEmbed.client.navigation.back();
+    console.log('2', this.router.currentRouteName);
+  }
+}
