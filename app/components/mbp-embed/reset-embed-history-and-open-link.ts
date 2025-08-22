881feb9ce2c9f8d4826a293f1ee0e52b2ff20242
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-08-22 08:24:33 +0200

    debug: await the .back + catch with alert when failed
    Refs: bnb-943

--- app/components/mbp-embed/reset-embed-history-and-open-link.ts
+++ app/components/mbp-embed/reset-embed-history-and-open-link.ts
@@ -3,8 +3,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 
-import { timeout } from 'ember-concurrency';
-
 import type MbpEmbedService from 'frontend-burgernabije-besluitendatabank/services/mbp-embed';
 import type RouterService from '@ember/routing/router-service';
 import type { FiltersAsQueryParams } from 'frontend-burgernabije-besluitendatabank/controllers/agenda-items/types';
@@ -33,11 +31,12 @@ export default class MbpEmbedResetEmbedHistoryAndOpenLink extends Component<MbpE
 
   async openNewEmbedWhenOverviewPage() {
     for (let view = 0; view < this.mbpEmbed.openViews; view++) {
-      this.mbpEmbed.client.navigation.back();
-      await timeout(120);
+      await this.mbpEmbed.client.navigation
+        .back()
+        .catch((e) => alert(JSON.stringify(e)));
     }
-    this.router.transitionTo(this.args.routeName, {
-      queryParams: this.args.query,
-    });
+    // this.router.transitionTo(this.args.routeName, {
+    //   queryParams: this.args.query,
+    // });
   }
 }
