2c82ee5b156647c11e25d7d0f8d8d9a994691d8f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-09-15 13:11:28 +0200

    feat: show the back button for the agendapunt in the session the same as the other back navigation
    Refs: bnb-980

--- app/controllers/agenda-items/session.ts
+++ app/controllers/agenda-items/session.ts
@@ -1,10 +1,16 @@
 import Controller from '@ember/controller';
+
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
 import type { ModelFrom } from '../../lib/type-utils';
 import { sortObjectsByTitle } from 'frontend-burgernabije-besluitendatabank/utils/array-utils';
 import type AgendaItem from 'frontend-burgernabije-besluitendatabank/models/agenda-item';
 import type AgendaItemsAgendaItemSessionRoute from 'frontend-burgernabije-besluitendatabank/routes/agenda-items/session';
+import type RouterService from '@ember/routing/router-service';
 
 export default class AgendaItemsAgendaItemSessionController extends Controller {
+  @service declare router: RouterService;
   /** Used to fetch agenda items from the model */
   declare model: ModelFrom<AgendaItemsAgendaItemSessionRoute>;
 
@@ -21,5 +27,13 @@ export default class AgendaItemsAgendaItemSessionController extends Controller {
       .slice()
       .filter(({ title }) => !!title)
       .sort(sortObjectsByTitle);
+  }
+
+  @action
+  toAgendapunt() {
+    this.router.transitionTo(
+      'agenda-items.agenda-item',
+      this.model.agendaItem.id,
+    );
   }
 }
